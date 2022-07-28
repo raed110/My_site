2f6fd21c06eb625417f90abed286e74827290281
raed110 <78529463+raed110@users.noreply.github.com>
2022-07-28 17:03:30 +0300

    Add files via upload

---
+++ js/translateFile.js
@@ -0,0 +1,159 @@
+var arrLang = new Array();
+arrLang['en'] = new Array();
+arrLang['ar'] = new Array();
+
+// English navbar content
+arrLang['en']['menu'] = 'Menu';
+arrLang['en']['home'] = 'Home';
+arrLang['en']['about'] = 'About Me';
+arrLang['en']['contact'] = 'Contact Me';
+arrLang['en']['resume'] = 'My Resume';
+arrLang['en']['edu'] = 'Education';
+arrLang['en']['skills'] = 'Skills';
+arrLang['en']['services'] = 'My Services';
+
+
+// Arabic navbar content
+arrLang['ar']['menu'] = 'القائمة';
+arrLang['ar']['home'] = 'الرئيسية'
+arrLang['ar']['about'] = 'عني';
+arrLang['ar']['contact'] = 'التواصل';
+arrLang['ar']['resume'] = 'سيرتي'
+arrLang['ar']['edu'] = 'التعليم';
+arrLang['ar']['skills'] = 'مهاراتي';
+arrLang['ar']['services'] = 'خدماتي';
+
+
+//English home section
+arrLang['en']['greating'] = 'Hey! I am'
+arrLang['en']['myName'] = 'Raed Al-Subhi'
+arrLang['en']['myRole'] = 'Bachelor’s student at Umm Al-Qura University, College of Computers and Information Systems, Department of Information Systems, Front End Developer'
+
+// //Arabic home section
+arrLang['ar']['greating'] = 'مرحبا ، أنا '
+arrLang['ar']['myName'] = 'رائد الصبحي'
+arrLang['ar']['myRole'] = 'طالب بكالوريس في جامعة أم القرى ، بكلية الحاسب الآلي ونظم المعلومات ، بقسم نظم المعلومات ، مصمم الواجهات الأمامية'
+
+//English about me section
+arrLang['en']['aboutMe'] = 'About Me'
+arrLang['en']['headOfAbout'] = 'simple person'
+arrLang['en']['bodyOfAbout'] = " just waking up early to perform the Fajr prayer in congregation. This makes me the happiest person on earth. I love sports of all kinds, I enjoy reading in my spare time I prefer self-development books over novels, and finally I like to challenge myself who I am today and who I will be tomorrow"
+
+
+//Arabic about me section
+arrLang['ar']['aboutMe'] = 'من أنا ؟'
+arrLang['ar']['headOfAbout'] = 'إنسان بسيط'
+arrLang['ar']['bodyOfAbout'] = 'مجرد الإستيقاظ مبكرًا لأداء صلاة الفجر في جماعة. هذا يجعلني أسعد إنسان على وجه الأرض. أحب الرياضة بجميع أنواعها ، أستمتع بالقراءة في وقت فراغي أفضل كتب تطوير الذات على الروايات ، وأخيراً أحب أن أتحدى نفسي من أنا اليوم ومن سأكون غدًا'
+
+
+//English resume section
+//Education section
+arrLang['en']['education'] = 'Education'
+
+
+arrLang['en']['MasterDate'] = '2023 - 2024 (Expected)'
+arrLang['en']['Master'] = "Bachelor's degree in Information Systems"
+arrLang['en']['KAU'] = 'Umm Al-Qura University'
+
+
+//Arabic resume section
+//Education section
+arrLang['ar']['education'] = 'التعليم'
+
+
+arrLang['ar']['MasterDate'] = '2023 - 2024 (متوقع التخرج)'
+arrLang['ar']['Master'] = 'درجة البكالوريوس في نظم المعلومات'
+arrLang['ar']['KAU'] = 'جامعة أم القرى'
+
+
+//English skills section
+arrLang['en']['skills'] = 'Skills'
+
+arrLang['en']['Programing'] = 'Programing'
+arrLang['en']['Discussion'] = 'Dialogue and Discussion Management'
+arrLang['en']['speek'] = 'Public Speak'
+arrLang['en']['Self'] = 'Self Learning'
+arrLang['en']['teaching'] = 'English'
+arrLang['en']['sketch'] = 'Acquire a new skill'
+
+
+
+//Arabic skills section
+arrLang['ar']['skills'] = 'مهاراتي'
+
+arrLang['ar']['Programing'] = 'البرمجة'
+arrLang['ar']['Discussion'] = 'إدارة الحوار والنقاش'
+arrLang['ar']['speek'] = 'الإلقاء والتقديم'
+arrLang['ar']['Self'] = 'التعلم الذاتي'
+arrLang['ar']['teaching'] = 'اللغة الإنجليزية'
+arrLang['ar']['sketch'] = 'اكتساب مهارة جديدة'
+
+
+
+//English Services section
+arrLang['en']['Services'] = 'Services'
+
+arrLang['en']['Teaching'] = 'Create a database'
+arrLang['en']['TeachingDescription'] = 'Are you facing a data problem ?? I can solve it for you by creating a database using {MY MSQ}'
+
+arrLang['en']['web'] = 'Web Developer'
+arrLang['en']['webDescription'] = 'Do you like my personal website? I can develop one for you.'
+
+arrLang['en']['EV3'] = 'Java programming '
+arrLang['en']['EV3Description'] = 'Do you want to learn how to Java programming ? I can help you.'
+
+//Arabic services section
+arrLang['ar']['Services'] = 'خدمات أستطيع تقدميها لك'
+
+arrLang['ar']['Teaching'] = 'عمل قاعدة بيانات'
+arrLang['ar']['TeachingDescription'] = 'هل تواجه مشكلة في البيانات ؟؟ يمكن حلها من أجلك عن طريق إنشاء قاعدة بيانات باستخدام {MY MSQ}'
+
+arrLang['ar']['web'] = 'تطوير المواقع'
+arrLang['ar']['webDescription'] = 'هل أعجبك تصميم موقعي الشخصي؟ بإمكاني تطوير وتصميم موقعاً لك '
+
+
+arrLang['ar']['EV3'] = 'برمجة الجافا'
+arrLang['ar']['EV3Description'] = 'هل لديك اهتمام بتعلم أساسيات برمجة الجافا؟ يسعدني مساعدتك في ذلك'
+
+
+//English contact section
+arrLang['en']['send'] = 'Send Message'
+
+arrLang['en']['contactMe'] = 'Contact Me'
+arrLang['en']['Address'] = 'Address'
+arrLang['en']['makkah'] = 'Saudi Arabia, Makkah'
+arrLang['en']['number'] = 'Telegram Account'
+arrLang['en']['email'] = 'Email Address'
+arrLang['en']['thanks'] = 'Thanks for contacting Me! I will get back to you soon!'
+
+
+
+//Arabic contact section
+arrLang['ar']['send'] = 'ارسل'
+
+arrLang['ar']['contactMe'] = 'للتواصل معي'
+arrLang['ar']['Address'] = 'العنوان'
+arrLang['ar']['makkah'] = 'المملكة العربية السعودية ، مكة المكرمة '
+arrLang['ar']['number'] = 'تيلقرام'
+arrLang['ar']['email'] = 'البريد الالكتروني'
+arrLang['ar']['thanks'] = 'شكراً لتواصلك ، سيتم الرد عليك قريباً إن شاء الله'
+
+//ِEnglish footer section
+arrLang['en']['maillist'] = "Monthly, you'll get some of inspiring and motivating ideas and some reviews about my favorite books."
+arrLang['en']['Subscribe'] = 'Subscribe'
+arrLang['en']['thanksSubscribe']= 'Thanks for subscription! '
+
+//Arabic footer section
+arrLang['ar']['maillist'] = 'شهرياً ستصلك رسائل بأفكار ملهمة ومحفزة وبعض المراجعات لأهم الكتب التي قرأتها'
+arrLang['ar']['Subscribe'] = 'اشتراك'
+arrLang['ar']['thanksSubscribe']= 'شكراً لاشتراكك'
+// Process translation
+$(function() {
+  $('.translate').click(function() {
+    var lang = $(this).attr('id');
+
+    $('.lang').each(function(index, item) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    });
+  });
+});
